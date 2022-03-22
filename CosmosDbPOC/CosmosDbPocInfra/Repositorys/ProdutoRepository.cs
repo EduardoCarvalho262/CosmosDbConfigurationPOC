d2464513ac295cbf355f8cbba16e95f01bd80e84
Eduardo <eduardoc313@gmail.com>
2022-03-21 21:56:47 -0300

    criando produto repository

---
+++ CosmosDbPOC/CosmosDbPocInfra/Repositorys/ProdutoRepository.cs
@@ -0,0 +1,19 @@
+﻿using CosmosDbPOC.Core.Models;
+using CosmosDbPocInfra.Interfaces;
+using CosmosDbPocInfra.Interfaces.CosmosDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosDbPocInfra.Repositorys
+{
+    public class ProdutoRepository : CosmosDbRepository<Produto>, IProdutoRepository
+    {
+
+        public ProdutoRepository(ICosmosDbClientFactory factory) : base(factory){ }
+        public override string CollectionName { get; } = "";
+
+    }
+}
